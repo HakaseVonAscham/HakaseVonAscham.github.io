ac5ea040c4fea8031e1e0c47936df82ef4a10058
Axel Godoy <axellvg@outlook.es>
2025-01-15 20:13:02 +0100

    Started css rework
    New css style rework started

--- js/main.js
+++ js/main.js
@@ -13,3 +13,16 @@ function Main() {
         backDelay : 1500,
     })
 }
+
+/*===== Scroll Reveal Animation*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200
+});
+
+sr.reveal('.index-card',{});
+/*setTimeout(function() {
+    window.location.href = "html/main.html";
+}, 50000);*/
