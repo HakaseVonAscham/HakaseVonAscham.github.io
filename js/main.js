76c02e20f3fa6112da8139c6dd421c464bbc5f60
Axel Godoy <axellvg@outlook.es>
2025-01-29 20:21:32 +0100

    Minor Changes
    Main JS
       New function for index
    Index CSS
       Fixed index background
    Index HTML
       Change for main js call

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+/*===== Scroll Reveal Animation*/
+function LoadIndexAnimation() {
+    const sr = ScrollReveal({
+        origin: 'top',
+        distance: '60px',
+        duration: 2000,
+        delay: 200
+    });
+
+    sr.reveal('.index-card',{});
+    
+    setTimeout(function() {
+        window.location.href = "html/main.html";
+    }, 5000);
+}
+
 function Main() {
     let banner = document.querySelector('.banner')
     let dayNight = document.querySelector('.dayNight')
@@ -13,16 +29,3 @@ function Main() {
         backDelay : 1500,
     })
 }
-
-/*===== Scroll Reveal Animation*/
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
-    duration: 2000,
-    delay: 200
-});
-
-sr.reveal('.index-card',{});
-/*setTimeout(function() {
-    window.location.href = "html/main.html";
-}, 50000);*/
